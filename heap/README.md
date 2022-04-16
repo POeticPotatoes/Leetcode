4b4e678b8916a213c2576e6cf9cf8dc1221899f9
POeticPotatoes <poeticpotatoes@gmail.com>
2022-04-17 01:02:31 +0800

    Add README.md to heap group

---
+++ heap/README.md
@@ -0,0 +1,2 @@
+# Heap Solutions
+Solutions to practices provided in leetcode's "Explore heap" module, in python with heapq
--- heap/k_weakest_rows.py
+++ heap/k_weakest_rows.py
@@ -2,6 +2,7 @@ import heapq
 
 class Solution:
     def kWeakestRows(self, nums, k):
+
         return "Not Implemented"
 
     def helloWorld(self):
